23783a4995379d33a4d7e0a32fff1b6006b5f93d
Darky <179386711+Darky-Github@users.noreply.github.com>
2025-07-16 20:01:40 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ankalc-cache-v1";
+const FILES_TO_CACHE = [
+  "/ankalc/",
+  "/ankalc/index.html",
+  "/ankalc/manifest.json",
+  "/ankalc/icons/icon-192.png",
+  "/ankalc/icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
